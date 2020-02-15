18e414795b597520652957830e4946fa6a3c561b
Alan-hit <419968612@qq.com>
2020-02-15 18:15:55 +0800

    更新2月15日数据

--- ncov/data/temp.py
+++ ncov/data/temp.py
@@ -4,9 +4,9 @@ import pandas as pd
 df = pd.read_csv('allcity_2020{}.csv'.format(datetime.date.today().strftime('%m%d')))
 beijing = df.loc[(df['provincename'] == '北京') & (df['cityname'] == '全市')]
 beijing = beijing[['confirmed', 'suspected', 'dead', 'cured', 'time']]
-beijing.to_csv('beijing_截至{}_24时.csv'.format(datetime.date.today().strftime('%m%d')), index=None)
+beijing.to_csv('beijing_截至{}_24时.csv'.format((datetime.date.today() + datetime.timedelta(days=-1)).strftime('%m%d')), index=None)
 
 shanghai = df.loc[(df['provincename'] == '上海') & (df['cityname'] == '全市')]
 shanghai = shanghai[['confirmed', 'suspected', 'dead', 'cured', 'time']]
-shanghai.to_csv('shanghai_截至{}_24时.csv'.format(datetime.date.today().strftime('%m%d')), index=None)
+shanghai.to_csv('shanghai_截至{}_24时.csv'.format((datetime.date.today() + datetime.timedelta(days=-1)).strftime('%m%d')), index=None)
 
